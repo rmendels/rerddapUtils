31fe6d16903e2b30d85a479752db0690a3018830
Roy Mendelssohn <roy.mendelssohn@noaa.gov>
2024-02-28 14:56:43 -0800

    refactor of converson.R
    refactored.  Added more tests to align file crs and user crs if given

--- R/conversion.R
+++ R/conversion.R
@@ -38,50 +38,43 @@
 #'  )
 #' test <- xy_to_latlon(proj_extract, yName = 'ygrid', xName = 'xgrid')
 xy_to_latlon <- function (resp, yName = 'cols', xName = 'rows', crs = NULL) {
-  # resp is call to rerddap::griddap() check that it is the right type
+  # Validate resp is from rerddap and extract necessary attributes
+  if (!inherits(resp, c('griddap_nc', 'rxtracto', 'rxtracto3D', 'rxtractoTrack'))) {
+    stop("The response object is not recognized. Please provide a valid 'griddap', 'rxtracto', 'rxtracto3D', or 'rxtractoTrack' object.")
+  }
+
   url <- attributes(resp)$url
   datasetid <- attributes(resp)$datasetid
-  base_loc <- stringr::str_locate(url, 'erddap')
-  base_loc <- base_loc[1, 2]
+  base_loc <- stringr::str_locate(url, 'erddap')[1, 2]
   base_url <- substr(url, 1, (base_loc + 1))
   dataInfo <- rerddap::info(datasetid, url = base_url)
-  proj_strings <- c('proj4string', 'proj_crs_code', 'proj4text', 'projection',
-                    'grid_mapping_epsg_code','grid_mapping_proj4',
-                    'grid_mapping_proj4_params', 'grid_mapping_proj4text',
-                    'WKT')
 
+  proj_strings <- c('proj4string', 'proj_crs_code', 'proj4text', 'projection',
+                    'grid_mapping_epsg_code', 'grid_mapping_proj4',
+                    'grid_mapping_proj4_params', 'grid_mapping_proj4text', 'WKT')
+
+  # Determine CRS
   if (!is.null(crs)) {
-    proj_crs_code = crs
+    proj_crs_code <- crs
   } else {
-    crs_test <- intersect( proj_strings, dataInfo$alldata$NC_GLOBAL$attribute_name)
+    crs_test <- intersect(proj_strings, dataInfo$alldata$NC_GLOBAL$attribute_name)
     if (length(crs_test) == 0) {
-      print('Could not find any of folowing fields for crs information')
-      print( proj_strings)
-      stop('please find crs info and provide in function call')
-    }  else {
-      proj_crs_code_index <- which(dataInfo$alldata$NC_GLOBAL$attribute_name == crs_test[1] )
+      stop("Could not find CRS information in the dataset. Please provide CRS information in the function call.")
+    } else {
+      proj_crs_code_index <- match(crs_test, dataInfo$alldata$NC_GLOBAL$attribute_name)
       proj_crs_code <- dataInfo$alldata$NC_GLOBAL$value[proj_crs_code_index]
     }
   }
-  # where the data reside depend on whether griddap, rxtracto, rxtracto3D
-  if (inherits(resp, 'griddap_nc')) {
-    temp_df <- data.frame(xgrid = resp$data[[xName]], ygrid = resp$data[[yName]])
-  }  else if (inherits(resp, 'rxtracto3D')) {
-       temp_df <- data.frame(xgrid = resp[[xName]], ygrid = resp[[yName]])
-  }  else if (inherits(resp, 'rxtractoTrack')) {
-       temp_df <- data.frame(xgrid = resp[[xName]], ygrid = resp[[yName]])
-  }  else {
-       print('the response does not appear to be from  any of')
-       print('griddap, rxtracto, rxtracto_3D')
-       stop('double-check your function call')
-  }
+  temp_df <- extract_grid_data(resp, xName, yName)
 
-  temp_df <- sf::st_as_sf(temp_df, coords = c('xgrid', 'ygrid'), crs = proj_crs_code)
+  # Convert grid data to spatial data frame and transform to lat-lon
+  temp_df <- sf::st_as_sf(temp_df, coords = c('xgrid', 'ygrid'), crs = proj_crs_code[1])
   temp_df <- sf::st_transform(temp_df, crs = 4326)
   coordinates <- sf::st_coordinates(temp_df)
   dimnames(coordinates)[[2]] <- c('longitude', 'latitude')
+
   coordinates
-  }
+}
 
 #' Convert Latitude-Longitude to Projected Coordinates
 #'
@@ -122,25 +115,56 @@ latlon_to_xy <- function (dataInfo, latitude, longitude, yName = 'latitude', xNa
                     'grid_mapping_epsg_code','grid_mapping_proj4',
                     'grid_mapping_proj4_params', 'grid_mapping_proj4text',
                     'WKT')
-  if (!is.null(crs)) {
-    proj_crs_code = crs
-  } else {
-    crs_test <- intersect( unlist(proj_strings), unlist(dataInfo$alldata$NC_GLOBAL$attribute_name))
-    if (length(crs_test) == 0) {
-      print('Could not find any of folowing fields for crs information')
-      print( proj_strings)
-      stop('please find crs info and provide in function call')
-    }  else {
-      proj_crs_code_index <- which(dataInfo$alldata$NC_GLOBAL$attribute_name == crs_test[1] )
-      proj_crs_code <- dataInfo$alldata$NC_GLOBAL$value[proj_crs_code_index]
-    }
+  crs_given <- !is.null(crs)
+  crs_test <- intersect(unlist(proj_strings), unlist(dataInfo$alldata$NC_GLOBAL$attribute_name))
+  crs_in_file <- length(crs_test) > 0
+
+  # Initialize proj_crs_code
+  proj_crs_code <- NULL
+
+  # Determine CRS code from file if available
+  if (crs_in_file) {
+    proj_crs_code_index <- match(crs_test, dataInfo$alldata$NC_GLOBAL$attribute_name)
+    proj_crs_code <- dataInfo$alldata$NC_GLOBAL$value[proj_crs_code_index]
   }
-  temp_df <- data.frame(Lat = latitude, Lon = longitude)
-  # EPSG:4326 is basic lat-lon coordinates
-  temp_df <- sf::st_as_sf(temp_df, coords = c("Lon", "Lat"), crs = 'EPSG:4326')
-  # project data
-  temp_df <- sf::st_transform(temp_df, crs = proj_crs_code)
-  # get projection coordinates
-  coordinates <- sf::st_coordinates(temp_df)
+
+  # Handle different cases
+  if (!crs_in_file && !crs_given) {
+    print("No CRS in file and no CRS given")
+    print("Look in dataset metadata for CRS information")
+    stop("Missing CRS information")
+  } else if (crs_in_file && !crs_given) {
+    # CRS in file, no CRS given
+    coordinates <- prepare_and_transform(proj_crs_code[1])
+  } else if (crs_in_file && crs_given) {
+    # CRS in file, CRS given
+    crs_agree <- crs %in% proj_crs_code
+    if (!crs_agree) {
+      warning("The CRS given does not agree with the CRS in the file")
+      warning(paste("The given CRS is", crs, "- the file CRS is", proj_crs_code))
+    }
+    coordinates <- prepare_and_transform(crs)
+  } else if (!crs_in_file && crs_given) {
+    # No CRS in file, CRS given
+    coordinates <- prepare_and_transform(crs)
+  }
+
   return(coordinates)
 }
+
+# Extract grid data based on the type of response object
+extract_grid_data <- function(resp, xName, yName) {
+  if (inherits(resp, 'griddap_nc')) {
+    data.frame(xgrid = resp$data[[xName]], ygrid = resp$data[[yName]])
+  } else {
+    data.frame(xgrid = resp[[xName]], ygrid = resp[[yName]])
+  }
+}
+
+prepare_and_transform <- function(crs_code) {
+  temp_df <- data.frame(Lat = latitude, Lon = longitude)
+  temp_df <- sf::st_as_sf(temp_df, coords = c("Lon", "Lat"), crs = 'EPSG:4326')
+  temp_df <- sf::st_transform(temp_df, crs = crs_code)
+  return(sf::st_coordinates(temp_df))
+}
+
